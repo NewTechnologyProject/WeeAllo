8761cb57cfeb454d20e56c3c2855d5e3e82d8b9e
Tuan Khang <you@example.com>
2021-09-14 09:42:33 +0700

    finish set up client server test api

--- client/src/services/api.service.js
+++ client/src/services/api.service.js
@@ -1,11 +1,9 @@
-/* eslint-disable import/no-anonymous-default-export */
 import { Feedback } from "@material-ui/icons";
 import axios from "axios";
 
 const baseApi = "http://localhost:4000/api/"
-
 export default {
-    user(url = baseApi + 'user/get-all-users') {
+    user(url = baseApi + "user/get-all-users") {
         return {
             fetchAll: () => axios.get(url),
         }
